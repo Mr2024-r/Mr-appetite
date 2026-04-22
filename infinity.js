d8d852ddd66cf3cb75a2201419289315251adc77
Mr2024-r <mrappetiterestaurant@gmail.com>
2026-04-22 13:21:37 +0300

    Add files via upload

---
+++ infinity.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    let menu = document.getElementById("dropdown");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function order() {
+    alert("Item added! Continue ordering via WhatsApp or Email.");
+}
